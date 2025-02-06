8837823f2740a1f9a071a0eab608b7a7b937ce70
Pilca <alexandru.pilca@codingmonkeys.net>
2025-02-06 20:17:38 +0100

    Update CARS.sql

--- Cars Project/CARS.sql
+++ Cars Project/CARS.sql
@@ -16,42 +16,44 @@ VALUES (3, 'Honda', 'Civic', TO_DATE('10-11-2020', 'DD-MM-YYYY'), 'Black', 4);
 
 COMMIT;
 
-CREATE OR REPLACE FUNCTION UPDATE_CAR(
-     P_ID CARS.CAR_ID%TYPE,  
-    P_MODEL CARS.MODEL%TYPE,
-    P_DATE_OF_MANUF CARS.DATE_OF_MANUFACTURE%TYPE  
-)
+DECLARE
+    V_UPDATE_YEAR NUMBER;
+    
+BEGIN
+    DECLARE
+        V_CAR_ROW CARS%ROWTYPE;
+    
+        FUNCTION UPD_CAR(
+            P_ID CARS.CAR_ID%TYPE,  
+            P_MODEL CARS.MODEL%TYPE,
+            P_DATE_OF_MANUF CARS.DATE_OF_MANUFACTURE%TYPE  
+        ) RETURN NUMBER
+        AS
+        BEGIN
+            V_CAR_ROW.CAR_ID := P_ID;
+            V_CAR_ROW.MODEL := P_MODEL;
+            V_CAR_ROW.DATE_OF_MANUFACTURE := P_DATE_OF_MANUF;
 
-RETURN NUMBER
-AS
- V_YEAR NUMBER;
- 
- BEGIN
-   UPDATE CARS 
-   SET 
-   MODEL = P_MODEL,
-   DATE_OF_MANUFACTURE =  P_DATE_OF_MANUF
-   WHERE CAR_ID = P_ID;
-   
-   SELECT EXTRACT(YEAR FROM DATE_OF_MANUFACTURE)
-   INTO V_YEAR
-   FROM CARS 
-   WHERE CAR_ID = P_ID;   
- 
- RETURN V_YEAR;
- END UPDATE_CAR;
- /
- 
- DECLARE
- 
- V_UPDATE_YEAR NUMBER;
- 
- BEGIN
- V_UPDATE_YEAR := UPDATE_CAR(P_ID => 1,
-                             P_MODEL => 'Yaris',
-                             P_DATE_OF_MANUF => TO_DATE('01-05-2023', 'DD-MM-YYYY'));
-                             
- DBMS_OUTPUT.PUT_LINE( 'Updated model and year of manufacture: '|| V_UPDATE_YEAR);
- 
- END;
- /
+            UPDATE CARS 
+            SET 
+                MODEL = V_CAR_ROW.MODEL,
+                DATE_OF_MANUFACTURE = V_CAR_ROW.DATE_OF_MANUFACTURE
+            WHERE CAR_ID = V_CAR_ROW.CAR_ID;
+
+            SELECT EXTRACT(YEAR FROM DATE_OF_MANUFACTURE)
+            INTO V_CAR_ROW.DATE_OF_MANUFACTURE
+            FROM CARS 
+            WHERE CAR_ID = V_CAR_ROW.CAR_ID;   
+
+            RETURN EXTRACT(YEAR FROM V_CAR_ROW.DATE_OF_MANUFACTURE);
+        END UPD_CAR;
+    
+    BEGIN
+        V_UPDATE_YEAR := UPDATE_CAR(P_ID => 1,
+                                    P_MODEL => 'Yaris',
+                                    P_DATE_OF_MANUF => TO_DATE('01-05-2023', 'DD-MM-YYYY'));
+                                    
+        DBMS_OUTPUT.PUT_LINE('Updated model and year of manufacture: ' || V_UPDATE_YEAR);
+    END;
+END;
+/
