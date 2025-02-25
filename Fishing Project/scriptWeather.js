353c2436da3c367a719cdf1fe7f18af6f0d376c5
AlexandruPilca <alexandru.pilca@codingmonkeys.net>
2025-02-25 12:59:43 +0100

    upd fishing app

--- Fishing Project/scriptWeather.js
+++ Fishing Project/scriptWeather.js
@@ -20,14 +20,13 @@ async function checkWeather(city) {
       console.log(data);
 
       document.querySelector(".city").innerHTML = data.name;
-      document.querySelector(".temp").innerHTML =
-        Math.round(data.main.temp) + "°C";
+      document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
       document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
       document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 
-      let currentTime = data.dt; 
-      let sunriseTime = data.sys.sunrise; 
-      let sunsetTime = data.sys.sunset; 
+      let currentTime = data.dt;
+      let sunriseTime = data.sys.sunrise;
+      let sunsetTime = data.sys.sunset;
       let isNight = currentTime < sunriseTime || currentTime > sunsetTime;
 
       if (data.weather[0].main === "Clouds") {
@@ -35,13 +34,21 @@ async function checkWeather(city) {
           ? "images/cloudy (1).png"
           : "images/cloudy.png";
       } else if (data.weather[0].main === "Clear") {
-        weatherApp.weatherIcon.src = isNight ? "images/crescent-moon.png" : "images/sun.png";
+        weatherApp.weatherIcon.src = isNight
+          ? "images/crescent-moon.png"
+          : "images/sun.png";
       } else if (data.weather[0].main === "Snow") {
-        weatherApp.weatherIcon.src = isNight ? "images/snow (1).png" : "images/snow.png";
+        weatherApp.weatherIcon.src = isNight
+          ? "images/snow (1).png"
+          : "images/snow.png";
       } else if (data.weather[0].main === "Rain") {
-        weatherApp.weatherIcon.src = isNight ? "images/thunder.png" : "images/storm.png";
-      }else if (data.weather[0].main === "Mist") {
-        weatherApp.weatherIcon.src = isNight ? "images/mist.png" : "images/mist.png";
+        weatherApp.weatherIcon.src = isNight
+          ? "images/thunder.png"
+          : "images/storm.png";
+      } else if (data.weather[0].main === "Mist") {
+        weatherApp.weatherIcon.src = isNight
+          ? "images/mist.png"
+          : "images/mist.png";
       }
       document.querySelector(".weather").style.display = "block";
       document.querySelector(".error").style.display = "none";
